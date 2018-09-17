ea4e12930718786bb136d3668be7de047c809a63
Bipin <bipin.sd@gmail.com>
2018-09-17 20:04:39 +0530

    adding joda

--- src/com/bee/joda/JodaTest.java
+++ src/com/bee/joda/JodaTest.java
@@ -1,7 +1,6 @@
 package com.bee.joda;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.*;
 
 public class JodaTest {
     public static void main(String args[]) {
@@ -12,6 +11,26 @@ public class JodaTest {
         System.out.println(date.getDayOfMonth());
         System.out.println(date.getMonthValue());
         System.out.println(date.getYear());
+        System.out.println(date.plusMonths(6l));
+        System.out.println(date.minusMonths(6l));
 
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt); //2018-09-16T20:00:11.681
+
+        LocalDate bday = LocalDate.of(1984, Month.NOVEMBER, 13);
+        Period p = Period.between(bday, date);
+        System.out.println(p.getYears()+"-"+p.getMonths()+"-"+p.getDays());
+
+        //LocalDateTime.of(yy, mm, dd, h, m, s, n)
+
+        Year y = Year.of(1984);
+        System.out.println(y.isLeap());
+        System.out.println(Year.of(1985).isLeap());
+
+        ZoneId zoneId = ZoneId.systemDefault();
+        System.out.println(zoneId);
+
+        ZoneId laId = ZoneId.of("America/Los_Angeles");
+        System.out.println(ZonedDateTime.now(laId));
     }
 }
